1dbbaee2a267d1ebd0ce8d9141104400591ddda8
nessrine <nessrine.chammakhi.simplon@gmail.com>
2020-05-05 13:53:18 +0100

    jsx1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Paragraphe from './paragraphe/paragraphe';
+
+function App() {
+  return (
+    <div className="App">
+     <Paragraphe/>
+     <a href="https://www.google.com"><h1>Google</h1></a>
+     <h2>REACT</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/paragraphe/paragraphe.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./paragraphe.css";
+
+
+
+function Paragraphe() {
+  return (
+
+   
+    <div className="paragraphe">
+
+    
+     <p>I'm a paragrapher</p>
+      
+   
+    </div>
+    
+  );
+}
+
+export default Paragraphe;
